c958bf51646512f048bfde458e903d505054efba
KunzxD404 <91834999+KunzxD404@users.noreply.github.com>
2021-12-07 19:38:44 +0700

    Add files via upload

--- message/group.js
+++ message/group.js
@@ -1,4 +1,5 @@
 module.exports = welcome = async (iky, anu) => {
+const { MessageType } = require("@adiwajshing/baileys")
 const fs = require("fs-extra")
 const { color, bgcolor } = require('../lib/color')
 const { getBuffer } = require('../lib/myfunc')
@@ -19,26 +20,29 @@ pp_grup = await iky.getProfilePicture(anu.jid)
 } catch (e) {
 pp_grup = 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png?q=60'
  }
- if (anu.action == 'add' && !mem.includes(iky.user.jid)) {
- if (!welkom.includes(anu.jid)) return
-mdata = await iky.groupMetadata(anu.jid)
+if (anu.action == 'add' && !mem.includes(iky.user.jid)) {
 num = anu.participants[0]
-let v = iky.contacts[num] || { notify: num.replace(/@.+/, '') }
-anu_user = v.vname || v.notify || num.split('@')[0]
-teks = `*Hi @${mem.split('@')[0]}*\n*Welcome in group ${mdata.subject}*\n*Jangan Jadi Kutu Lomcat Ya~~*`
-buff = await getBuffer(ppimg)
-iky.sendMessage(mdata.id, { contentText: `${teks}`, footerText: `Jangan Lupa Baca Rules Group Ya🐣`, buttons: [{buttonId: `#rulesgroup`,buttonText:{displayText: 'Rules Group'},type:1},{buttonId: `#menu`,buttonText:{displayText: 'Menu'},type:1}],headerType: 'LOCATION', locationMessage: { degreesLatitude: '', degreesLongitude: '', jpegThumbnail: buff, contextInfo: {"mentionedJid": [num]}}}, 'buttonsMessage')
+mdata = await iky.groupMetadata(anu.jid)
+try {
+ppimg = await iky.getProfilePicture(`${anu.participants[0].split('@')[0]}@c.us`)
+} catch {
+ppimg = 'https://i0.wp.com/www.gambarunik.id/wp-content/uploads/2019/06/Top-Gambar-Foto-Profil-Kosong-Lucu-Tergokil-.jpg'
 }
-if (anu.action == 'remove' && !mem.includes(iky.user.jid)) {
-if (!welkom.includes(anu.jid)) return
-mdata = await iky.groupMetadata(anu.jid)
+teks = `*Hi @${num.split('@')[0]}*\n*Welcome in group ${mdata.subject}*\n*Jangan Jadi Kutu Lomcat Ya~~*`
+let buffer = await getBuffer(`http://hadi-api.herokuapp.com/api/card/welcome?nama=${num.split('@')[0]}&descriminator=${mdata.participants.length}&memcount=${mdata.participants.length}&gcname=${mdata.subject}&pp=${ppimg}&bg=https://telegra.ph/file/ec7859cd2bc54b8d6b313.jpg`)
+iky.sendMessage(mdata.id, buffer, MessageType.image, { caption: teks, contextInfo: { "mentionedJid": [num] } })
+} else if (anu.action == 'remove') {
 num = anu.participants[0]
-let w = iky.contacts[num] || { notify: num.replace(/@.+/, '') }
-anu_user = w.vname || w.notify || num.split('@')[0]
-memeg = mdata.participants.length
-out = `*Selamat Tinggal* *@${num.split('@')[0]}*\n*Semoga Tenang Di Alam Sana*`
-buff = await getBuffer(ppimg)
-iky.sendMessage(mdata.id, { contentText: `${out}`, footerText: `Mari Kita Doakan Bersama² Agar Jasadnya Tenang😀`, buttons: [{buttonId: `!bay`,buttonText:{displayText: 'BYE KAK👋'},type:1}],headerType: 'LOCATION', locationMessage: { degreesLatitude: '', degreesLongitude: '', jpegThumbnail: buff, contextInfo: {"mentionedJid": [num]}}}, 'buttonsMessage')}
+mdata = await iky.groupMetadata(anu.jid)
+try {
+ppimg = await iky.getProfilePicture(`${num.split('@')[0]}@c.us`)
+} catch {
+ppimg = 'https://i0.wp.com/www.gambarunik.id/wp-content/uploads/2019/06/Top-Gambar-Foto-Profil-Kosong-Lucu-Tergokil-.jpg'
+}
+teks = `*Sayonara Onechan* *@${num.split('@')[0]}👋*`
+let buffer = await getBuffer(`http://hadi-api.herokuapp.com/api/card/welcome?nama=${num.split('@')[0]}&descriminator=${mdata.participants.length}&memcount=${mdata.participants.length}&gcname=${mdata.subject}&pp=${ppimg}&bg=https://telegra.ph/file/ec7859cd2bc54b8d6b313.jpg`)
+iky.sendMessage(mdata.id, buffer, MessageType.image, { caption: teks, contextInfo: { "mentionedJid": [num] } })
+}
 } catch (e) {
 console.log('Error : %s', color(e, 'red'))
 }
